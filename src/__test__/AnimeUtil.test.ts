a8a46676649a6a62ac18224afb7851aa248dd1e8
Chris <christianto.g.14@gmail.com>
2020-08-22 14:19:09 +0700

    test skeleton

---
+++ src/__test__/AnimeUtil.test.ts
@@ -0,0 +1,24 @@
+import { getAnimeId, getAnimeUrl } from "../AnimeUtil";
+import InvalidUrl from "../exceptions/InvalidUrl";
+
+describe('Should be able to parse Anime URL', () => {
+    test('Normal URL ', () => {
+        expect(getAnimeId('https://myanimelist.net/anime/24405/World_Trigger')).toBe(24405);
+        expect(getAnimeId('https://myanimelist.net/anime/6746')).toBe(6746);
+    })
+
+    test('Invalid Anime URL should NOT be resolvable', () => {
+        // Wrong base URL
+        expect(() => getAnimeId('https://myanimlist.net/anime/24405/World_Trigger')).toThrow(InvalidUrl);
+
+        // Wrong Path
+        expect(() => getAnimeId('https://myanimelist.net/anim/24405/World_Trigger')).toThrow(InvalidUrl);
+        expect(() => getAnimeId('https://myanimelist.net/anim/6746')).toThrow(InvalidUrl);
+    })
+})
+
+describe('should be able to generate Anime URL', () => {
+    test('with given ID', () => {
+        expect(getAnimeUrl(24405)).toBe('https://myanimelist.net/anime/24405')
+    })
+})
--- src/exceptions/InvalidUrl.ts
+++ src/exceptions/InvalidUrl.ts
@@ -1,1 +1,8 @@
-export default class InvalidUrl extends Error {}
+export default class InvalidUrl extends Error {
+    constructor(m: string) {
+        super(m);
+
+        // Set the prototype explicitly.
+        Object.setPrototypeOf(this, InvalidUrl.prototype);
+    }
+}
